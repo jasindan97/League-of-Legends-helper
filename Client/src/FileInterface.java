a3fbb8a5d553590b563ae31e1f9227753656564f
Jasindan Rasalingam <jasindan.rasalingam@ontariotechu.net>
2019-10-28 18:51:37 -0400

    upload completed project to git repo

---
+++ Client/src/FileInterface.java
@@ -0,0 +1,17 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Jasindan Rasalingam - 100584595
+ *
+ */
+public interface FileInterface extends Remote {
+   public byte[] downloadFile(String fileName) throws RemoteException;
+   public ArrayList<String> championBuildz(String champName, ArrayList<String> items) throws RemoteException;
+   public String champRolez() throws RemoteException;
+   public ArrayList<String> matchHistory(String summonerName) throws RemoteException;
+   public String teamStats() throws RemoteException;
+   public String champCounters(String champName) throws RemoteException;
+}
---
+++ Server/src/FileServer.java
@@ -0,0 +1,20 @@
+
+
+import java.io.*;
+import java.rmi.*;
+
+public class FileServer {
+    public static void main(String argv[]) {
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+        try {
+            FileInterface fi = new FileImpl("FileServer");
+            Naming.rebind("//127.0.0.1/FileServer", fi);
+            System.out.println("running");
+        } catch (Exception e) {
+            System.out.println("FileServer: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
